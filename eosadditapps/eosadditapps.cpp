33955eb9cd747ac876bc4a8962f9f0a237b7e926
VioletStair <violetstair@gmail.com>
2018-12-06 12:08:17 +0900

    Update: Mockup

--- eosadditapps/eosadditapps.cpp
+++ eosadditapps/eosadditapps.cpp
@@ -1,31 +1,35 @@
 #include "eosadditapps.hpp"
 
-ACTION eosadditapps::signup( string nickname, string avatar, string comment ) {
-    print_f("signup");
-}
-ACTION eosadditapps::update( string nickname, string avatar, string comment ) {
-    print_f("update");
-}
-ACTION eosadditapps::add( string url, string comment ) {
-    print_f("add");
-}
-ACTION eosadditapps::edit( uint64_t index, string comment ) {
-    print_f("edit");
-}
-ACTION eosadditapps::del( uint64_t index ) {
-    print_f("del");
-}
-ACTION eosadditapps::vote( uint64_t index, int8_t vote ) {
-    print_f("vote");
-}
-ACTION eosadditapps::create( asset maximum_supply ) {
-    print_f("create");
-}
-ACTION eosadditapps::issue( asset quantity, string memo ) {
-    print_f("issue");
-}
-ACTION eosadditapps::transfer( name from, name to, asset quantity, string memo ) {
-    print_f("transfer");
+ACTION eosadditapps::signup(name account, string nickname, string avatar, string memo) {
+    require_auth( account );
+    
+    print_f("eosadditapps::signup");
 }
 
-EOSIO_DISPATCH( eosadditapps, (signup)(update)(add)(edit)(del)(vote)(create)(issue)(transfer) )
+ACTION eosadditapps::update(name account, string nickname, string avatar, string memo) {
+    require_auth( account );
+    print_f("eosadditapps::update");
+}
+
+ACTION eosadditapps::add(name account, string url, string comment) {
+    require_auth( _self );
+    print_f("eosadditapps::add");
+}
+
+ACTION eosadditapps::edit(name account, uint64_t opidx, uint64_t coidx, string comment) {
+    print_f("eosadditapps::edit");
+}
+
+ACTION eosadditapps::del(name account, uint64_t opidx, uint64_t coidx) {
+    print_f("eosadditapps::del");
+}
+
+ACTION eosadditapps::vote(name account, uint64_t index, int8_t vote) {
+    print_f("eosadditapps::vote");
+}
+
+ACTION eosadditapps::repute(name account, string url, int8_t repute) {
+    print_f("eosadditapps::repute");
+}
+
+EOSIO_DISPATCH( eosadditapps, (signup)(update)(add)(edit)(del)(vote)(repute) )
--- eosadditapps/eosadditapps.hpp
+++ eosadditapps/eosadditapps.hpp
@@ -1,86 +1,86 @@
 #include <eosiolib/eosio.hpp>
-#include <eosiolib/crypto.h>
 #include <eosiolib/asset.hpp>
 #include <eosiolib/time.hpp>
-#include "multiindex/addit.hpp"
-#include "multiindex/token.hpp"
 
 using namespace eosio;
 using namespace std;
 
-CONTRACT eosadditapps : public eosio::contract {
+CONTRACT eosadditapps : public eosio::contract
+{
     private:
-        const char* tokensymbol = "ADDIT";
+        const char *tokensymbol = "ADDIT";
+        const int8_t UPVOTE = 1;
+        const int8_t DOWNVOTE = -1;
+
     public:
         using contract::contract;
-        eosadditapps( name receiver, name code, datastream<const char*> ds )
-            : contract(receiver, code, ds)
-            , opinion(receiver, receiver.value)
-            , user(receiver, receiver.value) {}
-
-        // account management
-        ACTION signup( string nickname, string avatar, string comment );
-        ACTION update( string nickname, string avatar, string comment );
-
-        // addit
-        ACTION add( string url, string comment );
-        ACTION edit( uint64_t index, string comment );
-        ACTION del( uint64_t index );
-        ACTION vote( uint64_t index, int8_t vote );
-
-        // token economy
-        ACTION create( asset maximum_supply );
-        ACTION issue( asset quantity, string memo );
-        ACTION transfer( name from, name to, asset quantity, string memo );
-
-        inline asset get_supply( symbol_code sym ) const;
-        inline asset get_balance( name owner, symbol_code sym ) const;
-
-        using add_action = action_wrapper<"add"_n, &eosadditapps::add>;
-        using edit_action = action_wrapper<"edit"_n, &eosadditapps::edit>;
-        using del_action = action_wrapper<"del"_n, &eosadditapps::del>;
-        using vote_action = action_wrapper<"vote"_n, &eosadditapps::vote>;
-        using create_action = action_wrapper<"create"_n, &eosadditapps::create>;
-        using issue_action = action_wrapper<"issue"_n, &eosadditapps::issue>;
-        using transfer_action = action_wrapper<"transfer"_n, &eosadditapps::transfer>;
+        eosadditapps(name receiver, name code, datastream<const char *> ds)
+            : contract(receiver, code, ds) {}
 
         TABLE user {
             name account;
             string nickname;
-            string comment;
+            string memo;
             string avatar;
+
             uint64_t primary_key()const { return account.value; }
         };
 
-        TABLE votelog {
+        TABLE voting {
             name account;
             int8_t vote;
             block_timestamp blocktime;
-            uint64_t primary_key()const { return account.value; }
+
+            uint64_t primary_key() const { return account.value; }
         };
+
+        TABLE reputation {
+            name account;
+            int8_t vote;
+            block_timestamp blocktime;
+
+            uint64_t primary_key() const { return account.value; }
+        };
+
         TABLE comments {
             uint64_t index;
             name account;
             string comment;
             uint32_t upvote;
             uint32_t downvote;
-            votelog voting;
+            voting vote;
             block_timestamp blocktime;
-            uint64_t primary_key()const { return index; }
+
+            uint64_t primary_key() const { return index; }
         };
+
         TABLE opinion {
             uint64_t index;
             string url;
-            capi_checksum512 url_hash;
             comments comment;
+            reputation repute;
+            int64_t repvalue;
             block_timestamp blocktime;
-            uint64_t primary_key()const { return index; }
+
+            uint64_t primary_key() const { return index; }
         };
 
-        typedef eosio::multi_index<"user"_n, user> users;
-        typedef eosio::multi_index<"opinion"_n, opinion> opinions;
+        ACTION signup(name account, string nickname, string avatar, string memo);
+        ACTION update(name account, string nickname, string avatar, string memo);
+        ACTION add(name account, string url, string comment);
+        ACTION edit(name account, uint64_t opidx, uint64_t coidx, string comment);
+        ACTION del(name account, uint64_t opidx, uint64_t coidx);
+        ACTION vote(name account, uint64_t index, int8_t vote);
+        ACTION repute(name account, string url, int8_t repute);
 
-    private:
-        opinions opinion;
-        users user;
+        using signup_action = action_wrapper<"signup"_n, &eosadditapps::signup>;
+        using update_action = action_wrapper<"update"_n, &eosadditapps::update>;
+        using add_action = action_wrapper<"add"_n, &eosadditapps::add>;
+        using edit_action = action_wrapper<"edit"_n, &eosadditapps::edit>;
+        using del_action = action_wrapper<"del"_n, &eosadditapps::del>;
+        using vote_action = action_wrapper<"vote"_n, &eosadditapps::vote>;
+        using repute_action = action_wrapper<"repute"_n, &eosadditapps::repute>;
+
+        using opinions_index = multi_index<"opinion"_n, opinion>;
+        using users_index = multi_index<"user"_n, user>;
 };
